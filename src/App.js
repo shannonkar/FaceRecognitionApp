8971671cd109ec53c7c5f7b1bc5adf4ac00e6566
Shannon Makenna <shannonkariuki@gmail.com>
2019-03-30 20:27:51 +0100

    added the face detection api feature and added navigation functionality

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
 import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
 import Rank from './components/Rank/Rank';
+import Logo from './components/Logo/Logo';
+
 import './App.css';
 
 
@@ -43,42 +47,75 @@ class App extends Component {
     super();
     this.state = {
       input: '',
-      imageUrl: ''
-
+      imageUrl: '',
+      box: {},
+      route: 'signin',
+      isSignedIn: false
     }
   }
-  onInputChange = (event) => {
+  calculateFaceLocation = (data) =>{
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+  displayFaceBox = (box) =>{
+    console.log(box);
+    this.setState({box : box});
+  }
+  onInputChange = (event) =>{
     this.setState({input: event.target.value});
   }
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input})
     console.log('click');
-    app.models.predict(
+    app.models
+    .predict(
       Clarifai.FACE_DETECT_MODEL, 
       this.state.input)
-    .then(
-    function(response) {
-      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-      // do something with response
-    },
-    function(err) {
-      // there was an error
+    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .catch(err => console.log(err));
+
+  }
+  onRouteChange = (route) =>{
+    if(route === 'signout'){
+      this.setState({isSignedIn: false})
+    } else if (route ==='home'){
+      this.setState({isSignedIn: true})
     }
-  );
+    this.setState({route: route});
   }
   render() {
+   const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
        <Particles className ='particles'
               params={ particlesOptions } />
-        <Navigation />
-       <Rank />
-        <ImageLinkForm 
-        onInputChange ={this.onInputChange}
-        onButtonSubmit ={this.onButtonSubmit}
-        />
-        <FaceRecognition imageUrl ={this.state.imageUrl}/>
-       {/*<Logo />*/}
+       
+        <Navigation isSignedIn = {isSignedIn} onRouteChange = {this.onRouteChange} />
+        {route === 'home' 
+        ? 
+         <div> 
+            <Rank />
+            <Logo />
+            <ImageLinkForm 
+            onInputChange ={this.onInputChange}
+            onButtonSubmit ={this.onButtonSubmit}
+            />
+            <FaceRecognition box ={box} imageUrl ={imageUrl}/>
+       </div>
+        : (
+            this.state.route ==='signin'
+            ?<Signin onRouteChange ={this.onRouteChange} />
+            : <Register onRouteChange ={this.onRouteChange} />
+           )
+           }
       </div>
      
     );
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -5,10 +5,10 @@ import './Logo.css';
 const Logo = () =>{
 	return (
 		<div className='ma4 mt0'>
-     <Tilt className="Tilt" options={{max : 25 }} style={{ height: 250, width: 250 }} >
- 			<div className="Tilt-inner"> 👽 </div>
-	</Tilt>
-    </div>
+     		<Tilt className="Tilt br2 shadow-2" options={{ max : 25 }} style={{ height: 250, width: 250 }} >
+ 				<div className="Tilt-inner"> 👽 </div>
+			</Tilt>
+   		 </div>
 		);
 }
 
